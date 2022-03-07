62959b308383a904a7f546bb98911f8820c61ffb
LeeMinjea <gabe0529@naver.com>
2022-03-07 23:48:57 +0900

    m1 mac debuging setting

--- c_language/sandbox.c
+++ c_language/sandbox.c
@@ -3,8 +3,21 @@
 #include <string.h>
 #include <stdlib.h>
 
+// make function two num add 
+int add(int a, int b)
+{
+    return a + b;
+}
+
 int main(){
-    
+    int a, b;
+    int result;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    result = add(a, b);
+
+    printf("%d + %d = %d\n", a, b, result);
 
     return 0;
 }
---
+++ debug.c
@@ -0,0 +1,15 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+    int a = 1;
+
+
+    printf("hello world");
+
+    a++;
+
+    return 0;
+}
