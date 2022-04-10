e76963ec504f604ee3530e168416c002079fc147
LeeMinjea <gabe0529@naver.com>
2022-04-11 02:01:24 +0900

    `

---
+++ Python/number_theory/assignment/hw2.py
@@ -0,0 +1,26 @@
+# make func gcd(a, b) that returns the gcd value d of a and b
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+# make func extended_gcd(a, b) that returns the gcd value d, two constants s and t which satisfy the equation d = s*a + t*b
+def extended_gcd(a, b):
+    if b == 0:
+        return a, 1, 0
+    else:
+        d, s, t = extended_gcd(b, a % b)
+        return d, t, s - (a // b) * t
+
+print("gcd(45, 75) =", gcd(45, 75))
+print("gcd(666, 1414) =", gcd(666, 1414))
+print("gcd(102, 222) =", gcd(102, 222))
+print("gcd(2**101+16, 2**202+8) =", gcd(2**101+16, 2**202+8))
+
+print("extended_gcd(45, 75) =", extended_gcd(45, 75))
+print("extended_gcd(666, 1414) =", extended_gcd(666, 1414))
+print("extended_gcd(102, 222) =", extended_gcd(102, 222))
+print("extended_gcd(2**101+16, 2**202+8) =", extended_gcd(2**101+16, 2**202+8))
+    
+    
--- Python/코딩과스토리텔링/week_6_assignment.py
+++ Python/코딩과스토리텔링/week_6_assignment.py
@@ -1,7 +1,4 @@
 # k-mooc : 코딩과 스토리텔링 6주차 과제
-from re import A
-
-
 menu = ["짜장면", "탕수육", "짬뽕", "양장피"]
 
 print("welcome to sejong-won")
@@ -10,7 +7,8 @@ print("1 : 메뉴보기\n2 : 주문하기")
 choice = int(input())
 
 if(choice == 1):
-    print("a : {}, b : {}, b : {}, c : {}".format(menu[0], menu[1], menu[2], menu[3]))
+    print("a : {}, b : {}, b : {}, c : {}".format(
+        menu[0], menu[1], menu[2], menu[3]))
 
 elif(choice == 2):
     print("주문하실 메뉴를 선택하세요.")
