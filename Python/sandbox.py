dfa91becfd04c02bca07f47b41c7e133ffcfcd90
LeeMinjea <gabe0529@naver.com>
2022-04-17 23:51:29 +0900

    num_theory

--- Python/sandbox.py
+++ Python/sandbox.py
@@ -1,29 +1,8 @@
-# k-mooc : 코딩과 스토리텔링 6주차 과제
-from re import A
-
-
-menu = ["짜장면", "탕수육", "짬뽕", "양장피"]
-
-print("welcome to sejong-won")
-
-print("1 : 메뉴보기\n2 : 주문하기")
-choice = int(input())
-
-if(choice == 1):
-    print("a : {}, b : {}, b : {}, c : {}".format(menu[0], menu[1], menu[2], menu[3]))
-
-elif(choice == 2):
-    print("주문하실 메뉴를 선택하세요.")
-    order = input()
-    if(order == 'a'):
-        print("짜장면을 주문하셨습니다.")
-    elif(order == 'b'):
-        print("탕수육을 주문하셨습니다.")
-    elif(order == 'c'):
-        print("짬뽕을 주문하셨습니다.")
-    elif(order == 'd'):
-        print("양장피를 주문하셨습니다.")
+#factorial
+def fac(x):
+    if x == 0:
+        return 1
     else:
-        print("잘못된 입력입니다.")
-else:
-    print("잘못된 입력입니다.")
+        return x * fac(x-1)
+
+print(fac(7))
