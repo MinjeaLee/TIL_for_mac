857099175ba3b28ca9562bbdc2f89a0075a625ff
LeeMinjea <gabe0529@naver.com>
2022-04-19 23:03:40 +0900

    baekjoon

--- c_language/sandbox.c
+++ c_language/sandbox.c
@@ -1,81 +1,31 @@
+#pragma warning(disable:4996)
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#pragma warning(disable : 4996)
-struct node
-{
-    int num;
-    struct node *next;
-};
-int subset(struct node *A, struct node *B)
-{
-    int flag = 1;
-    struct node *tmp = A;
-    struct node *tmp2 = B;
-    if (tmp->next == NULL)
-    {
-        return 0;
+
+int main(){
+    int n, m;
+    char chess_board[50][50];
+
+    int count = 0;
+    int max_count = -1;
+    int max_1d_index, max_2d_index;
+
+    scanf("%d %d", &n, &m);
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            scanf("%c", &chess_board[i][j]);
+        }
     }
 
-    else
-    {
-        tmp2 = tmp2->next;
-        tmp = tmp->next;
-        while (1)
-        {
-            if (tmp2 == NULL)
-            {
-                break;
-            }
-            if (tmp2->num == tmp->num)
-            {
-                tmp = tmp->next;
-                if (tmp == NULL)
-                {
-                    flag = -1;
-                    break;
-                }
-            }
-            tmp2 = tmp2->next;
-        }
-        if (flag == 1)
-        {
-            return tmp->num;
-        }
-        else if (flag == -1)
-        {
-            return 0;
+    for(int i = 0; i < n - 8; i++){
+        for(int j = 0; j < m - 8; j++){
+            
         }
     }
-}
-int main()
-{
-    int A_size;
-    struct node *A_header = (struct node *)malloc(sizeof(struct node) * 1);
-    A_header->next = NULL;
-    scanf("%d", &A_size);
-    struct node *tmp = A_header;
-    for (int i = 0; i < A_size; i++)
-    {
-        struct node *p = (struct node *)malloc(sizeof(struct node) * 1);
-        scanf("%d", &p->num);
-        tmp->next = p;
-        tmp = tmp->next;
-    }
-    tmp->next = NULL;
 
-    int B_size;
-    struct node *B_header = (struct node *)malloc(sizeof(struct node) * 1);
-    B_header->next = NULL;
-    scanf("%d", &B_size);
-    struct node *tmp2 = B_header;
-    for (int i = 0; i < B_size; i++)
-    {
-        struct node *p = (struct node *)malloc(sizeof(struct node) * 1);
-        scanf("%d", &p->num);
-        tmp2->next = p;
-        tmp2 = tmp2->next;
-    }
-    tmp2->next = NULL;
-    printf("%d", subset(A_header, B_header));
+
+
     return 0;
 }
