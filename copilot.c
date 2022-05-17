c9e9d777ab7bf0e7194fbd3209bcdd9723a4fca2
LeeMinjea <gabe0529@naver.com>
2022-05-17 16:08:58 +0900

    study argc argv

--- copilot.c
+++ copilot.c
@@ -1,2 +1,10 @@
-// do you have a girlfriend? 
-//  
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+    printf("argc = %d\n", argc);
+    for(int i = 0; i < argc; i++){
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+
+}
