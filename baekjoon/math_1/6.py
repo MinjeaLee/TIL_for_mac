b1d364c46a4356f37a73ef1494a8e3b50edf7c74
LeeMinjea <gabe0529@naver.com>
2023-02-17 20:59:37 +0900

    TODO

--- baekjoon/math_1/6.py
+++ baekjoon/math_1/6.py
@@ -17,3 +17,20 @@ for i in range(t):
 		print(n)
 	else:
 		print(sum(floor[k - 1]))
+
+# chatGPT 코드
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    k = int(sys.stdin.readline())  # 층
+    n = int(sys.stdin.readline())  # 호수
+
+    people = [[i for i in range(1, n+1)]]
+    for i in range(1, k+1):
+        row = [sum(people[i-1][:j+1]) for j in range(n)]
+        people.append(row)
+
+    print(people[k][n-1])
+# 나랑 무엇이 다른 지 잘 모르겠음,,,ㅠㅠ
+#TODO : 무엇이 다른지 파악 -> 코드 수정
