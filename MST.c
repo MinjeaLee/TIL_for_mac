5418ddef6eec02a305a9800e1c15125dd6f945ad
LeeMinjea <gabe0529@naver.com>
2023-08-08 23:04:30 +0900

    MOD : atexit, leaks 테스트 코드

--- MST.c
+++ MST.c
@@ -1,26 +1,8 @@
-#include <stdio.h>
+void f(){
+	system("leaks a.out");
+}
 
-int main()
-{
-	int num1, num2;
-	char pls;
-
-	scanf("%d ", &num1);
-	scanf("%d ", &num2);
-	scanf("%c", &pls);
-
-	switch (pls)
-	{
-	case '+':
-		printf("%d + %d = %d", num1, num2, num1 + num2);
-	case '-':
-		printf("%d - %d = %d", num1, num2, num1 - num2);
-	case '*':
-		printf("%d * %d = %d", num1, num2, num1 * num2);
-	case '/':
-		printf("%d / %d = %d", num1, num2, num1 / num2);
-	case '%':
-		printf("%d %%  %d = %d", num1, num2, num1 % num2);
-	}
-	return 0; 
+int main(){
+	/* code */
+	atexit(f);
 }
